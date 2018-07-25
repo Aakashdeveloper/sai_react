3c89dcdca5642c02807e94b5843f5e4519c336f2
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-07-25 17:53:49 +0530

    a

---
+++ firstapp/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './components/header'
+
+const App = () => {
+    return( 
+            <div>
+                <h1>Hello Sai</h1>
+                <Header/>
+            </div>)
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
+
+
+
+
+
+/*function add(a,b){
+    return a+b
+}
+
+var add = (a,b) => { return a+b }
+
+functional=> light weight less functinality
+
+class based=> bit hevay but more functionlity
+State
+Props
+
+let
+var
+const
+
+rendering=> MAin component
+export=> uslitiesed in main
+
+jsx
+
+*/
