3dfec7d79264ce2136c779b918d9aacb7219ee4d
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-08-27 06:55:25 +0530

    a

--- third_app/src/components/artist_list.js
+++ third_app/src/components/artist_list.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ArtistList = (props) => {
-    console.log(props)
-    return(
-        <div className="banner">
-            <span>hiii</span>
+const ArtistList =(props) =>{
+  
+
+    const list = ({allArtists}) =>{
+        if(allArtists){
+            return allArtists.map((item)=>{
+
+                const style ={
+                    background:`url('/images/covers/${item.cover}.jpg')
+                    no-repeat`
+                }
+                return(
+                    <Link key={item.id} to={`/artist/${item.id}`}
+                        className="artist_item" style={style}>
+                        <div> {item.name}</div>
+                    </Link>
+                )
+            })
+        }
+      
+    }
+
+   return(
+       <div className="artist_list">
+           <h1> Browse Artist Data</h1>
+           {list(props)}
         </div>
-    )
+   )
 }
 
 export default ArtistList;
--- third_app/src/index.js
+++ third_app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './components/home';
-import Artist from './components/home';
+import Artist from './components/artist';
 
 const App = () => {
     return(
